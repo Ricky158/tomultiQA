6300cd722ae9d680cda696482c6a673c8dc2b952
Ricky158 <52778594+Ricky158@users.noreply.github.com>
2021-06-20 16:38:21 +0800

    Delete tomultiqa directory

--- tomultiqa/ValueClass.java
+++
@@ -1,8 +0,0 @@
-package com.example.android.tomultiqa;
-
-public class ValueClass {
-    public static final String NORMAL_MODE = "Normal";
-    public static final String GAME_MODE = "Game";
-    public static final String FOCUS_MODE = "Focus";
-    public static final int CONFLICT_MAX_FLOOR = 25;
-}
